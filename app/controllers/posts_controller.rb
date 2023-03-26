c85f995fa9bc798a5cabd37274fe82bd3fa2c9f7
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-26 20:20:08 +0300

    complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,12 @@ class PostsController < ApplicationController
 
   def index
     posts = Post.all
-    render json: posts
+    render json: posts, include: ['profile', 'posts', 'posts.tags']
   end
 
   def show
     post = Post.find(params[:id])
-    render json: post
+    render json: post, include: ['profile', 'posts', 'posts.tags']
   end
 
   private
